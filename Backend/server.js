073428725d4b92e3e343e2c1032de62c758f1df6
PriyankaStudent <priyankamuthu0310@gmail.com>
2024-12-18 10:50:33 +0530

    Add cors and body-parser dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", 
-  password: "31March@2005", 
-  database: "employee_management", 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,        
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
